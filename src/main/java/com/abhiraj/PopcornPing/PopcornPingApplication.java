cd905bac8ac4727629c039d5d9f7402ff5a51bc2
abhirajsingh <abhirajsinghchouhan4@gmail.com>
2026-05-20 23:25:30 +0530

    entities, dtos and repositories configured

---
+++ src/main/java/com/abhiraj/PopcornPing/PopcornPingApplication.java
@@ -0,0 +1,13 @@
+package com.abhiraj.PopcornPing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornPingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornPingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhiraj/PopcornPing/PopcornPingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhiraj.PopcornPing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornPingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
